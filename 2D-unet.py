74d072b08e71ce17f38acbfda85a2451abc65dcb
mrkolarik <32169045+mrkolarik@users.noreply.github.com>
2019-06-12 18:02:16 +0200

    Corrected name of the pooling layer

--- 2D-unet.py
+++ 2D-unet.py
@@ -47,19 +47,19 @@ def get_unet():
     inputs = Input((img_rows, img_cols, 1))
     conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
     conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
-    pool1 = MaxPooling2DD(pool_size=(2, 2))(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
     conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
     conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
-    pool2 = MaxPooling2DD(pool_size=(2, 2))(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
     conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
     conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
-    pool3 = MaxPooling2DD(pool_size=(2, 2))(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
     conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
     conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
-    pool4 = MaxPooling2DD(pool_size=(2, 2))(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
     conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
     conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
